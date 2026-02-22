37bd20963189685a38e315825b5cf1300fb75d8a
MehmetHooke <hoke628@gmail.com>
2026-02-23 01:08:33 +0300

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import "./global.css";
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
@@ -17,6 +18,8 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="(user)" options={{ presentation: 'modal', title: 'User' }} />
+        <Stack.Screen name="(auth)" options={{ presentation: 'modal', title: 'Auth' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
